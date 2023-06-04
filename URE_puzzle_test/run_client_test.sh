447be558efe6173054c6b1a340baeb3191388ea9
Xiaojian Wang <461005496@qq.com>
2023-06-04 01:56:07 -0400

    Add files via upload

--- URE_puzzle_test/run_client_test.sh
+++ URE_puzzle_test/run_client_test.sh
@@ -1,6 +1,6 @@
-python3 URE_puzzle_client_test.py --puzzle_number 10 --iteration_times 200
-python3 URE_puzzle_client_test.py --puzzle_number 20 --iteration_times 200
-python3 URE_puzzle_client_test.py --puzzle_number 30 --iteration_times 200
-python3 URE_puzzle_client_test.py --puzzle_number 40 --iteration_times 200
-python3 URE_puzzle_client_test.py --puzzle_number 50 --iteration_times 200
+python3 URE_puzzle_client_test.py --puzzle_number 10 --iteration_times 1000
+python3 URE_puzzle_client_test.py --puzzle_number 20 --iteration_times 1000
+python3 URE_puzzle_client_test.py --puzzle_number 30 --iteration_times 1000
+python3 URE_puzzle_client_test.py --puzzle_number 40 --iteration_times 1000
+python3 URE_puzzle_client_test.py --puzzle_number 50 --iteration_times 1000
 # python3 URE_puzzle_client_test.py --puzzle_number 30 --iteration_times 200
